93b6cb90002c11bb6b5c75e4fc5bf0ed0eb0b662
Terry Smith <terry@codecov.io>
2020-10-08 12:16:41 -0300

    Removed a test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,6 @@
 import Index from "./index";
 
-test('test uncovered if', () => {
-    const indexObj = new Index();
-    expect(indexObj.uncovered_if()).toEqual(false);
-});
-
-test('fully covered', () => {
-    const indexObj = new Index();
-    expect(indexObj.fully_covered()).toEqual(true);
+test("fully covered", () => {
+  const indexObj = new Index();
+  expect(indexObj.fully_covered()).toEqual(true);
 });
